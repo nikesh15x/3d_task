ad371711293b4fcf5ab96a959418ad87cd587659
nikesh15x <talk2singhnikesh65@gmail.com>
2023-08-29 12:43:35 +0530

    completed object oriented q6

---
+++ pythonTask/ObjectOriented.py
@@ -0,0 +1,17 @@
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def calArea(self):
+        return self.length * self.width
+
+    def calPerimeter(self):
+        return 2 * (self.length + self.width)
+
+
+react = Rectangle(8.5, 6.8)
+
+print("Area of Rectangle is :",react.calArea())
+print("Perimeter of Rectangle is :",react.calPerimeter())
+
