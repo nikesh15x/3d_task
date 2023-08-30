912ffdf9da1d5607de88f7112bccf96c48dce77b
nikesh15x <talk2singhnikesh65@gmail.com>
2023-08-30 15:59:09 +0530

    Initial backend setup

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+require("./Config/Db/mongoConfig");
+const express = require("express");
+const bodyParser = require("body-parser");
+const port = process.env.PORT || 9090;
+const cors = require("cors");
+
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors({
+  origin: 'http://localhost:3000'
+}))
+
+//api's
+// const api = require('./Routes/index');
+// app.use('/', api);
+
+
+app.get("/", (req, res) => {
+  res.send("app working");
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
+
+module.exports = app;
