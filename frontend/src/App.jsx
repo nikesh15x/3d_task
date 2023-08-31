ba044694c5c716094bd6df0dd52c103cfca2d7af
nikesh15x <talk2singhnikesh65@gmail.com>
2023-08-31 08:28:04 +0530

    added tailwindcss and basic routes setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,24 @@
-import { useState } from 'react'
+import { useState } from "react";
+import Nav from "./Components/Navbar";
+import Home from "./pages/Home/Home";
+import Login from "./pages/auth/Login";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
     <>
-
+      <div>
+        <Nav />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<h1>Page not found</h1>} />
+          </Routes>
+        </main>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
